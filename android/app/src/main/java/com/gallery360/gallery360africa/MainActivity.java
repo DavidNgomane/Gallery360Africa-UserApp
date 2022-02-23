3bc57e722ed8eba1eb2b9f5ee6e2819a1998e18b
David Ngomane <70548142+Dngomane@users.noreply.github.com>
2022-02-23 17:15:39 +0200

    updated package name

--- android/app/src/main/java/com/gallery360/gallery360africa/MainActivity.java
+++ android/app/src/main/java/com/gallery360/gallery360africa/MainActivity.java
@@ -1,4 +1,4 @@
-package com.gallery360;
+package com.gallery360.gallery360africa;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Gallery360";
+    return "gallery360africa";
   }
 }
