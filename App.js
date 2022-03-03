4e7a35ed6e82b12427867d69af5a8da6e5fc5671
David Ngomane <70548142+Dngomane@users.noreply.github.com>
2022-03-03 15:12:18 +0200

    Update App.js

--- App.js
+++ App.js
@@ -164,6 +164,7 @@ const uuid = auth()?.currentUser?.uid;
             
             headerBackVisible: false,
             headerShadowVisible: false,
+            
             title: `Hi ${fullName}`,
             headerRight: () => (
               <View style={{flexDirection: 'row', width: 74, justifyContent: 'space-between'}}>
@@ -195,7 +196,20 @@ const uuid = auth()?.currentUser?.uid;
 			<Stack.Screen options={{headerShown: true,  headerTransparent: true}} name='DeliveryAddress' component={DeliveryAddress} />
 			<Stack.Screen options={{headerShown: true,  headerTransparent: true}} name='ShippingAddress' component={ShippingAddress} />
 			<Stack.Screen options={{headerShown: false}} name='Preview' component={Preview} />
-			<Stack.Screen options={{headerShown: true,  headerTransparent: true}} name='ExhibitionDetails' component={ExhibitionDetails} />
+			<Stack.Screen options={{
+        
+        headerShown: true,  
+        headerTransparent: true, 
+        headerTitleStyle: {
+       color: '#fff'
+      },
+      headerBackTitleStyle: {
+        color: '#fff'
+      },
+      }} 
+      name='ExhibitionDetails' component={ExhibitionDetails} 
+      />
+
 			<Stack.Screen options={{headerShown: true,  headerTransparent: true}} name='UserProfile' component={UserProfile} />
 			<Stack.Screen options={{headerShown: false}} name='Map' component={Map} /> 
 			<Stack.Screen options={{headerShown: false}} name='Exhibition' component={Exhibition} /> 
